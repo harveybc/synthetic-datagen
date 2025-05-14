04b85f6c3f778269a76f956a5e610f32e4d41abd
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-14 08:06:37 -0500

    Update phase_4_1_datagen.sh

--- examples/scripts/phase_4_1_datagen.sh
+++ examples/scripts/phase_4_1_datagen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Dgenerate synthetic data for phase 4_1 daily transformer configurations
-RESULTS_DIR="examples/data/phase_4_1"
+RESULTS_DIR="examples/data/phase_4"
 
 # Process the JSON configuration files(TODO: Fix that n_samples must be the same as max_steps_train in the code)
 sh sdg.sh  --n_samples 12600 --max_steps_train 25200 \
@@ -12,13 +12,5 @@ sh sdg.sh  --n_samples 25200 --max_steps_train 25200 \
 sh sdg.sh  --n_samples 50400 --max_steps_train 25200 \
     --output_file "$RESULTS_DIR/normalized_d4_25200_synthetic_50400_prepended.csv" \
     --metrics_file "$RESULTS_DIR/normalized_d4_25200_synthetic_50400_metrics.csv"
-
-
-
-
-sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_3150_1d_config.json"
-sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_6300_1d_config.json"
-sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_12600_1d_config.json"
-sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_25200_1d_config.json"
 
 echo All daily configurations processed.
