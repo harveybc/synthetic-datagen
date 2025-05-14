3086ae88dba58bd50f9af87553383af4e09e00c7
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-14 08:42:11 -0500

    Update phase_4_1_datagen.sh

--- examples/scripts/phase_4_1_datagen.sh
+++ examples/scripts/phase_4_1_datagen.sh
@@ -4,8 +4,8 @@ RESULTS_DIR="examples/data/phase_4"
 
 # Process the JSON configuration files(TODO: Fix that n_samples must be the same as max_steps_train in the code)
 sh sdg.sh  --n_samples 12600 --max_steps_train 25200 \
-    --output_file "$RESULTS_DIR/normalized_d4_25200_synthetic_3150_prepended.csv" \
-    --metrics_file "$RESULTS_DIR/normalized_d4_25200_synthetic_3150_metrics.csv" 
+    --output_file "$RESULTS_DIR/normalized_d4_25200_synthetic_12600_prepended.csv" \
+    --metrics_file "$RESULTS_DIR/normalized_d4_25200_synthetic_12600_metrics.csv" 
 sh sdg.sh  --n_samples 25200 --max_steps_train 25200 \
     --output_file "$RESULTS_DIR/normalized_d4_25200_synthetic_25200_prepended.csv" \
     --metrics_file "$RESULTS_DIR/normalized_d4_25200_synthetic_25200_metrics.csv"
