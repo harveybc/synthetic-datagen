6755d597600d3ce06f42272166ad7432621c0da9
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-27 20:51:00 -0500

    Update sdg.sh

--- sdg.sh
+++ sdg.sh
@@ -1,4 +1,13 @@
 export PREV_PYTHONPATH=$PYTHONPATH
 export PYTHONPATH=./:$PYTHONPATH
+
+echo "--- Debug from sdg.sh ---"
+echo "Which python: $(which python)"
+echo "Python version:"
+python --version
+echo "PYTHONPATH: $PYTHONPATH"
+echo "--- End Debug ---"
+
 python app/main.py "$@"
+
 export PYTHONPATH=$PREV_PYTHONPATH
