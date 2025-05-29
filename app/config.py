981554fc906f3557898df06a237858efc8c0400e
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-29 05:08:17 -0500

    update

--- app/config.py
+++ app/config.py
@@ -31,8 +31,8 @@ DEFAULT_VALUES = {
     "dataset_periodicity": "1h", 
 
      # Generation parameters
-    "n_samples": 50400,
-    "max_steps_train": 25200,
+    "n_samples": 5040,
+    "max_steps_train": 2520,
     "latent_shape": [18, 32], 
     "batch_size": 32, 
     
